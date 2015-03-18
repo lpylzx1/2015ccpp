6769411c8d7097a6980fab0b58dff8bfb3a1af60
lpylzx1 <lpylzx1@qq.com>
2015-03-18 22:43:53 +0800

    Updata Time  -- 2015.03.18 Version 0.2
    global variable: time -- delete
    revise function :revise --add &times

--- Move.c
+++ Move.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
-#include <algorithm>
-int time = 0;
 
-void Move(int num,int stackOne,int stackTwo,int stackThree)
-{
-    if (num == 1)
-    {
-        printf("stack %d->stack %d\n",stackOne,stackThree);
-        time++;
-    }
-    else
-    {
-        Move(num-1,stackOne,stackThree,stackTwo);
-        printf("stack %d->stack %d\n",stackOne,stackThree);
-        time++;
-        Move(num-1,stackTwo,stackOne,stackThree);
-    }
-
-}
+void Move(int num,int stackFrom,int stackBuffer,int stackTo,int &times);
 
 
 
 int main(int argc, char const *argv[])
 {
-    int num = 0;
-    const int one = 1, two = 2, three = 3;
+    int num = 0,times = 0;
+    const int from = 1, buffer = 2, to = 3;
     scanf("%d",&num);
-    Move(num,one,two,three);
-    printf("time is = %d \n",time);
+    Move(num,from,buffer,to,times);
+    printf("time is = %d \n",times);
     return 0;
+}
+
+
+
+
+void Move(int num,int stackFrom,int stackBuffer,int stackTo,int &times)
+{
+    if (num == 1)
+    {
+        printf("stack %d->stack %d\n",stackFrom,stackTo);
+        ++times;
+    }
+    else
+    {
+        Move(num-1,stackFrom,stackTo,stackBuffer,times);
+        printf("stack %d->stack %d\n",stackFrom,stackTo);
+        Move(num-1,stackBuffer,stackFrom,stackTo,++times);
+    }
 }
